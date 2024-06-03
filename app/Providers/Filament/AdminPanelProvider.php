8fd90fe722d9e2cc656fa5b23443841489849071
Adam <lv4mj1@gmail.com>
2024-06-03 12:06:29 +0100

    Update Admin Panel with Plugin

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -35,6 +35,9 @@ class AdminPanelProvider extends PanelProvider
             ->pages([
                 Pages\Dashboard::class,
             ])
+            ->plugins([
+                \FilipFonal\FilamentLogManager\FilamentLogManager::make(),
+            ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
